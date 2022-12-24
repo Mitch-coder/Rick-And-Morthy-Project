5424e53aec29deb7db9e32d5c4ee8432c4141c93
Mitch-coder <michellescalderon2002@gmail.com>
2022-12-24 17:57:39 -0600

    search context

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./routes";
 import { UserProvider } from "./providers/UserProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SearchProvider } from "./providers/SearchProvider";
+import { useState } from "react";
 
 const App = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+
+
   return (
     <UserProvider>
+      <SearchProvider >
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
+      </SearchProvider>
     </UserProvider>
   );
 };
